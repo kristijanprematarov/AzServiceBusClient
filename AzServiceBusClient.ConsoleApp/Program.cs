45b89cfca113eb34b63bb4a37873186e2f0fddcf
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2022-08-22 21:02:24 +0200

    Add project files.

---
+++ AzServiceBusClient.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string connectionString = "Endpoint=sb://kpr-servicebus.servicebus.windows.net/;SharedAccessKeyName=ReadWrite;SharedAccessKey=wKeIrWvOCJ8vlpunfuISPcg5djW8JxUC/A2PC9y7ULY=;";
+
+string queueName = "orderqueue";
+
+ServiceBusClient client = new ServiceBusClient(connectionString);
+
+ServiceBusSender sender = client.CreateSender(queueName);
+
+Console.WriteLine("Please enter your name:");
+string name = Console.ReadLine();
+
+await sender.SendMessageAsync(new ServiceBusMessage(@$"Message: {name}"));
